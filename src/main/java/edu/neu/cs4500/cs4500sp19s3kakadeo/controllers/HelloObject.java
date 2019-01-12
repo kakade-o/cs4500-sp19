40d6da1902711a5bc4ba54639a27d7e3f5e4756e
omkarkakade <omkar710@gmail.com>
2019-01-11 19:25:18 -0500

    first commit

---
+++ src/main/java/edu/neu/cs4500/cs4500sp19s3kakadeo/controllers/HelloObject.java
@@ -0,0 +1,17 @@
+package edu.neu.cs4500.cs4500sp19s3kakadeo.controllers;
+
+public class HelloObject {
+    private String message;
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public HelloObject(String message) {
+        this.message = message;
+    }
+    public HelloObject() {
+    }
+}
+
---
+++ src/test/java/edu/neu/cs4500/cs4500sp19s3kakadeo/Cs4500Sp19s3KakadeOApplicationTests.java
@@ -0,0 +1,17 @@
+package edu.neu.cs4500.cs4500sp19s3kakadeo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Cs4500Sp19s3KakadeOApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
